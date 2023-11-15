476dca3f9b4ee94c8a8a57fc8dbfa5946ea7c741
recannon <145130757+recannon@users.noreply.github.com>
2023-11-15 13:08:09 +0000

    update

--- Exercises2023/Ex1_2/Assignment/Assignment-1.cpp
+++ Exercises2023/Ex1_2/Assignment/Assignment-1.cpp
@@ -1,3 +1,22 @@
+// 20231115
+// Written by Richard Cannon
 
-// 20231115; Written by Richard Cannon
-//
+#include <iostream>
+#include <vector>
+
+int main(){
+
+// Print 'Hello World!'
+std::cout << "Hello World!" << std::endl;
+
+double x, y;
+
+x = 2.3;
+y = 4.5;
+std::vector<double> v;
+v[0] = x;
+v[1] = y;
+
+std::cout << v << std::endl;
+
+}
