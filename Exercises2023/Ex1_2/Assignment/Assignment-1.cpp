73e4e87e6edc2ca167c189dfc6909d600e8313b7
recannon <145130757+recannon@users.noreply.github.com>
2023-11-15 13:37:54 +0000

    Prelim answers 1 and 2

--- Exercises2023/Ex1_2/Assignment/Assignment-1.cpp
+++ Exercises2023/Ex1_2/Assignment/Assignment-1.cpp
@@ -3,20 +3,43 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
+
+double vectorMag(double, double);
 
 int main(){
 
-// Print 'Hello World!'
-std::cout << "Hello World!" << std::endl;
+    // 2 I
+    std::cout << "2 (I)" << std::endl;
+    std::cout << "Hello World! \n" << std::endl;
 
-double x, y;
+    // 2 II
+    double x, y;
+    x = 2.3;
+    y = 4.5;
 
-x = 2.3;
-y = 4.5;
-std::vector<double> v;
-v[0] = x;
-v[1] = y;
+    std::vector<double> v{x, y};
 
-std::cout << v << std::endl;
+    double z = pow((v[0]*v[0] + v[1]*v[1]),0.5);
 
+    std::cout << "2 (II)" << std::endl;
+    std::cout << "The magnitude of vector (x,y) is "
+    << z << std::endl << std::endl;
+
+    //2 (III)
+    double inp1, inp2;
+
+    std::cout << "Please enter the x and y components of a vector:\n"
+    << "Vx = "; 
+    std::cin >> inp1; 
+    std::cout << "\nVy = ";
+    std::cin >> inp2;
+    std::cout << std::endl;
+
+    double magnitude = vectorMag(inp1, inp2);
+
+    std::cout << "2 (III)" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return 0;
 }
