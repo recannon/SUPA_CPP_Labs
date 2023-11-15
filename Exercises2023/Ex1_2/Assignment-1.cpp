e0ec7c0fd0e290fe2f913a8424b44326a2f42271
recannon <145130757+recannon@users.noreply.github.com>
2023-11-15 15:08:19 +0000

    .

--- Exercises2023/Ex1_2/Assignment-1.cpp
+++ Exercises2023/Ex1_2/Assignment-1.cpp
@@ -8,30 +8,32 @@
 #include <cstdlib>
 
 int main() {
-
     std::cout << "Instructions 4.1" << std::endl;
-    std::ifstream data;
-    data.open("input2D_float.txt", std::ios::in);
-
-    data.clear();
-    data.seekg(0, std::ios::beg);
-
+    std::ifstream data_file;
     int line = 0;
+    data_file.open("/workspaces/SUPA_CPP_Labs/Exercises2023/Ex1_2/input2D_float.txt", std::ios::in);
+    if (data_file.fail()) {
+        std::cout << "Cannot find file \n";
+    }
+    else {
+        std::cout << "File found\n";
+        std::string headers;
+        double x = 4.56;
+        double y = 3.45;
+        
+        // Takes first line and saves it as header strings
+        data_file >> headers;
+        std::cout << headers << std::endl;
 
-    std::cout << line << std::endl;
-
-
-    //while (!data.eof()){
-      //  if (!data.eof()){
-       //     line++;
-       // }
-    //}
-    double x;
-    data >> x;
-
-    std::cout << x << std::endl;
-
-    std::cout << line << std::endl;
+        while (1) {
+            if(data_file.eof())          
+                break;  
+            data_file >> x >> y;              
+            std::cout<<x<<" "<<y<<std::endl; 
+            line++;
+            }
+        }
 
     std::cout << "\n\n";
+    return 0;
 }
