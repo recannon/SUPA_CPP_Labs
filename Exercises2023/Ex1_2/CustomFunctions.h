574b4028339211d616d91e122ad02f56743e0e13
recannon <145130757+recannon@users.noreply.github.com>
2023-11-19 09:09:28 +0000

    d

--- Exercises2023/Ex1_2/CustomFunctions.h
+++ Exercises2023/Ex1_2/CustomFunctions.h
@@ -3,7 +3,6 @@
 #include <cmath>
 #include <fstream>
 #include <cstdlib>
-#include "CustomFunctions.h"
 
 void read_data(std::ifstream&, std::vector<double>&, std::vector<double>&, int);
 void print_vector(std::vector<double>, int);
