743ac1743157b3637b9f128310bcf5126c6b3580
recannon <145130757+recannon@users.noreply.github.com>
2023-11-29 13:33:38 +0000

    .

---
+++ Exercises2023/Ex3_4/Assignment_2.cpp
@@ -0,0 +1,31 @@
+// Written by Richard Cannon
+// 20231129
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <cstdlib>
+#include <numeric>
+#include <math.h>
+#include <filesystem> //To check extensions in a nice way
+
+#include "gnuplot-iostream.h"
+#include "FiniteFunctions.h"
+
+int main() {
+    std::cout << "Hello world\n";
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    std::cout << "\n\n";
+}
